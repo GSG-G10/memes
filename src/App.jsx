6b960fd2c3300320327edb64ea7ddbb61111c328
ibrahim jomaa <78326620+divluffy@users.noreply.github.com>
2021-11-30 21:58:18 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,57 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 
+// @dev.luffy
+// @dev.luffy
+// @dev.luffy
+
 function App() {
   const [data, setData] = useState("");
-  const [reload, setReload] = useState(0);
-  useEffect(() => {
+
+  const getImg = () => {
     axios
       .get(
         "https://api.giphy.com/v1/gifs/random?api_key=uTbPKn6KPBJqplAZk1ijeqdMkaGC8umE"
       )
       .then((res) => {
-        console.log(res.data.data.images.original.url);
-        setData(res.data.data.images.original.url);
+        setData(res.data.data.images.downsized_medium.url);
       });
-  }, [reload]);
+  };
+
+  const changeImg = () => {
+    setData("");
+    getImg("");
+  };
+
+  useEffect(() => {
+    getImg();
+  }, []);
 
   return (
     <div className="wrapper">
-      <img src={data} alt="random img" />
-      <div
-        className="reload_img"
-        onClick={() => setReload((prev) => (prev += 1))}
-      >
+      {data ? (
+        <img src={data} alt="random img" />
+      ) : (
+        <div className="loading">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="512"
+            height="512"
+            viewBox="0 0 512 512"
+          >
+            <circle cx="256" cy="96" r="64" />
+            <circle cx="96" cy="256" r="48" />
+            <circle cx="368" cy="144" r="8" />
+            <path d="M180.1 107.6c-19.9-20.1-52.2-20.1-72.1 0-19.9 20.1-19.9 52.7 0 72.8 19.9 20.1 52.2 20.1 72.1 0 19.9-20.1 19.9-52.7 0-72.8z" />
+            <circle cx="416" cy="256" r="16" />
+            <circle cx="369" cy="369" r="24" />
+            <circle cx="256" cy="416" r="32" />
+            <circle cx="144" cy="368" r="40" />
+          </svg>
+        </div>
+      )}
+
+      <div className="reload_img" onClick={changeImg}>
         <svg
           focusable="false"
           viewBox="0 0 24 24"
